ca8e64f90c2d01211cf8d4fdc955cdce64f2c24e
Hitesh Mondal <mehiteshmondal@gmail.com>
2025-04-26 20:17:22 +0530

    new file:   src/components/CommunityList.tsx 	modified:   src/components/CreatePost.tsx 	modified:   src/pages/CreatePostPage.tsx

--- src/components/CreatePost.tsx
+++ src/components/CreatePost.tsx
@@ -1,50 +1,144 @@
-import { useState } from "react";
-import { useMutation } from "@tanstack/react-query";
+import { ChangeEvent, useState } from "react";
+import { useMutation, useQuery } from "@tanstack/react-query";
 import { supabase } from "../supabase-client";
+import { useAuth } from "../context/AuthContext";
+import { Community, fetchCommunities } from "./CommunityList";
 
 interface PostInput {
-    title: string;
-    content: string
+  title: string;
+  content: string;
+  avatar_url: string | null;
+  community_id?: number | null;
 }
-const createPost = async (post: PostInput) => {
-    const {data, error} = await supabase.from('posts').insert([post]);
-    if (error) {
-        throw error
-    }
-    return data
-}
+
+const createPost = async (post: PostInput, imageFile: File) => {
+  const filePath = `${post.title}-${Date.now()}-${imageFile.name}`;
+
+  const { error: uploadError } = await supabase.storage
+    .from("post-images")
+    .upload(filePath, imageFile);
+
+  if (uploadError) throw new Error(uploadError.message);
+
+  const { data: publicURLData } = supabase.storage
+    .from("post-images")
+    .getPublicUrl(filePath);
+
+  const { data, error } = await supabase
+    .from("posts")
+    .insert({ ...post, image_url: publicURLData.publicUrl });
+
+  if (error) throw new Error(error.message);
+
+  return data;
+};
 
 export const CreatePost = () => {
-    const [title, setTitle] = useState('');
-    const [content, setContent] = useState('');
+  const [title, setTitle] = useState<string>("");
+  const [content, setContent] = useState<string>("");
+  const [communityId, setCommunityId] = useState<number | null>(null);
 
-    const {mutate} = useMutation({mutationFn: createPost})    
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
 
-    const handleSubmit = (event: React.FormEvent) => {
-        event.preventDefault();
-        mutate({title, content})
+  const { user } = useAuth();
+
+  const { data: communities } = useQuery<Community[], Error>({
+    queryKey: ["communities"],
+    queryFn: fetchCommunities,
+  });
+
+  const { mutate, isPending, isError } = useMutation({
+    mutationFn: (data: { post: PostInput; imageFile: File }) => {
+      return createPost(data.post, data.imageFile);
+    },
+  });
+
+  const handleSubmit = (event: React.FormEvent) => {
+    event.preventDefault();
+    if (!selectedFile) return;
+    mutate({
+      post: {
+        title,
+        content,
+        avatar_url: user?.user_metadata.avatar_url || null,
+        community_id: communityId,
+      },
+      imageFile: selectedFile,
+    });
+  };
+
+  const handleCommunityChange = (e: ChangeEvent<HTMLSelectElement>) => {
+    const value = e.target.value;
+    setCommunityId(value ? Number(value) : null);
+  };
+
+  const handleFileChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      setSelectedFile(e.target.files[0]);
     }
+  };
 
-    const handleFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-        
-    }
+  return (
+    <form onSubmit={handleSubmit} className="max-w-2xl mx-auto space-y-4">
+      <div>
+        <label htmlFor="title" className="block mb-2 font-medium">
+          Title
+        </label>
+        <input
+          type="text"
+          id="title"
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+          className="w-full border border-white/10 bg-transparent p-2 rounded"
+          required
+        />
+      </div>
+      <div>
+        <label htmlFor="content" className="block mb-2 font-medium">
+          Content
+        </label>
+        <textarea
+          id="content"
+          value={content}
+          onChange={(e) => setContent(e.target.value)}
+          className="w-full border border-white/10 bg-transparent p-2 rounded"
+          rows={5}
+          required
+        />
+      </div>
 
-    return (
-        <form onClick={handleSubmit}>
-            <div>
-              <label>Title</label>
-              <input type="text" id="title" required onChange={(event) => setTitle(event.target.value)}/>
-            </div>
-            <div>
-              <label>Content</label>
-              <textarea id="content" required rows={6} onChange={(event) => setContent(event.target.value)}/>
-            </div>
-            <div>
-              <label>Upload Image</label>
-              <input type="file" id="image" accept="image/*" required onChange={handleFileChange}/>
-            </div>
-            
-            <button type="submit">Create Post</button>
-        </form>
-    );    
+      <div>
+        <label> Select Community</label>
+        <select id="community" onChange={handleCommunityChange}>
+          <option value={""}> -- Choose a Community -- </option>
+          {communities?.map((community, key) => (
+            <option key={key} value={community.id}>
+              {community.name}
+            </option>
+          ))}
+        </select>
+      </div>
+
+      <div>
+        <label htmlFor="image" className="block mb-2 font-medium">
+          Upload Image
+        </label>
+        <input
+          type="file"
+          id="image"
+          accept="image/*"
+          onChange={handleFileChange}
+          className="w-full text-gray-200"
+        />
+      </div>
+      <button
+        type="submit"
+        className="bg-purple-500 text-white px-4 py-2 rounded cursor-pointer"
+      >
+        {isPending ? "Creating..." : "Create Post"}
+      </button>
+
+      {isError && <p className="text-red-500"> Error creating post.</p>}
+    </form>
+  );
 };
