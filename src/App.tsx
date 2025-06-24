b7607af322a2e33c83942498addfcd336093778d
HiteshMondal <mehiteshmondal@gmail.com>
2025-06-24 20:42:49 +0530

    modified:   package-lock.json 	modified:   package.json 	modified:   src/App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,14 @@ import { CreateCommunityPage } from "./pages/CreateCommunityPage";
 import { CommunitiesPage } from "./pages/CommunitiesPage";
 import { CommunityPage } from "./pages/CommunityPage";
 
-
 function App() {
   return (
-    <div className="min-h-screen bg-black text-gray-100 transition-opacity duration-700 pt-20"
+    <div className="min-h-screen bg-black text-gray-100 transition-opacity duration-700 pt-20">
       <Navbar />
       <div className="container mx-auto px-4 py-6">
         <Routes>
-          <Route path="/" element={<Home />}/>
-          <Route path="/create" element={<CreatePostPage/>}/>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<CreatePostPage />} />
           <Route path="/post/:id" element={<PostPage />} />
           <Route path="/community/create" element={<CreateCommunityPage />} />
           <Route path="/communities" element={<CommunitiesPage />} />
@@ -23,8 +22,7 @@ function App() {
         </Routes>
       </div>
     </div>
-
-  );  
+  );
 }
 
-export default App
+export default App;
